723d92062de76422f37fc909d1837f5ad7109f13
GreyKL <351944950@qq.com>
2023-07-09 16:43:17 +0800

    test

---
+++ src/main/java/com/kl/fuckmidware/FuckMidwareApplication.java
@@ -0,0 +1,13 @@
+package com.kl.fuckmidware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckMidwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckMidwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kl/fuckmidware/FuckMidwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kl.fuckmidware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuckMidwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
